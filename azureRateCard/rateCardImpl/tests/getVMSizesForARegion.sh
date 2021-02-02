420122d12d657a55e42f23a4cd27dc16a4f254c2
Ninad.Kanthi <NI20125743@wipro.com>
2021-02-02 02:31:27 +0000

    Adding the bash script which has been linted, 100% complete coverage with automated testing

---
+++ azureRateCard/rateCardImpl/tests/getVMSizesForARegion.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# SET the REGION_NAME variable in the environment and don't pass it in the parameter. 
+# Huh. That might not be best option when we move all this to an Azure DevOPS pipeline. 
+# for the script to execute properly, following needs to be done one-time per terminal?
+# tenantID="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXX"
+# az login --tenant $tenantID
+
+# set default values
+regionName="westeurope"
+outputlocation="../output/"
+
+processDownloadingVMList(){
+    # At the time of writing this script, the accepted regions were
+    
+    # 'eastus, eastus2, westus, centralus, northcentralus, southcentralus, 
+    # northeurope, westeurope, 
+    # eastasia, southeastasia, 
+    # japaneast, japanwest, 
+    # australiaeast, australiasoutheast, australiacentral, 
+    # brazilsouth, 
+    # southindia, centralindia, westindia, 
+    # canadacentral, canadaeast, 
+    # westus2, westcentralus, 
+    # uksouth, ukwest, 
+    # koreacentral, koreasouth, 
+    # francecentral, 
+    # southafricanorth, uaenorth, 
+    # switzerlandnorth, 
+    # germanywestcentral, 
+    # norwayeast'
+    
+    if [ "$1" ];
+    then
+        regionName="$1"
+    fi
+
+    outputFileName="./AllVmsIn-$regionName.json"
+    echo "Region name set to $regionName. Getting the VM list..."
+
+    # Invoke the az function to get the actual list in json
+    az vm list-sizes --location "$regionName" --output json >> "$outputFileName"
+    echo "...done"
+
+    # check if the output happened ok
+    # read $outputFileName
+    if [[ -f "$outputFileName" && -s "$outputFileName" ]];
+    then
+        echo "SUCCESS: $outputFileName file created. Copying it to the $outputlocation folder"
+        # move the file to output folder
+        mv "$outputFileName" "$outputlocation"
+    else
+        echo "Error: $outputFileName Not found!"
+    fi
+}
+
+# first parameter passed is to set the subscription ID of the logged in 
+# Azure tenant. 
+if [ "$SUBSCRIPTION_ID" ];
+    then
+        az account set --subscription "$SUBSCRIPTION_ID"
+        processDownloadingVMList "$1"
+    else
+        echo "Need to pass Subscription ID as first parameter"
+fi
+
---
+++ azureRateCard/rateCardImpl/tests/testAll.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# container for testing all 
+
+baseDownloadFunctionImpl(){
+    # for the function to execute properly, following needs to be done one-time per terminal?
+    # tenantID="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXX"
+    # az login --tenant $tenantID
+    # extract the correct SubscriptionID and set that as environment variable 
+    # the key for the environment variable is SUBSCRPTION_ID
+
+    # default region name
+    regionName="westeurope"
+
+    # parameter passed is signal as to test for non-existing region and see what it results
+    if [ "$1" ];
+    then
+        regionName="nonexistingregion"
+    fi
+    # export REGION_NAME="$regionName"
+
+    outputlocation="../output/"
+    # export OUTPUT_LOCATION="$outputlocation"
+
+    # setup the TEST - ARRANGE
+    # notice that I do not pass the arguments to this command!!!
+    rm $outputlocation*.json
+
+    # execute the test - ACT
+    ./getVMSizesForARegion.sh $regionName
+
+    # if the original code changes, this will trigger an error in the test script. 
+    outputFileName="AllVmsIn-$regionName.json"
+    # check if the output happened ok
+    # ASSERT
+
+    if [ "$1" ];
+    then
+        # want to trigger the false selection. the file should not exist this time around. 
+        if [[ ! ( -f "$outputlocation$outputFileName" && -s "$outputlocation$outputFileName" ) ]];
+        then
+            assertEquals 1 1
+        fi
+    else
+        if [[ -f "$outputlocation$outputFileName" && -s "$outputlocation$outputFileName" ]];
+        then
+            assertEquals 1 1
+        fi
+    fi
+}
+
+testCorrectDownLoadOfVMList(){
+    baseDownloadFunctionImpl
+}
+
+
+testIncorrectDownloadOfVMList(){
+    baseDownloadFunctionImpl "passdummyVariable"
+}
+
+testUnsetSubscriptionIDFromEnv(){
+    # remove the SUBSCRIPTION_ID from the environment variable and then again execute the test
+    # expectatio is that the test should fail
+    unset SUBSCRIPTION_ID
+    testIncorrectDownloadOfVMList
+}
+
+. ./shunit2
+
+# rm *.csv
+# rm *.json
+# ./getVMSizesForARegion.sh
+# python3 allVMsInARegion.py
+# python3 getAzureVMListWithACUs.py
+# python3 getAzureRateCard.py
+# python3 combineACUandRateCardData.py
+# # optional
+# python3 testRateCard.py
