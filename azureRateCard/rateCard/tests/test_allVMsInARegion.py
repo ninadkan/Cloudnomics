85748a3d1a8130e76ca94e38e851db94ddfd482a
Ninad.Kanthi <NI20125743@wipro.com>
2021-02-09 12:52:51 +0000

    add pluggin to set correct order for python test execution

--- azureRateCard/rateCard/tests/test_allVMsInARegion.py
+++ azureRateCard/rateCard/tests/test_allVMsInARegion.py
@@ -11,6 +11,7 @@ from rateCard import getAzureVMListWithACUs as gaz
 import os
 import pandas as pd
 import logging
+import pytest
 
 outputPath = os.environ['CLOUDNOMICS_OUTPUT_PATH']
 inputFileName = "AllVmsIn-" + os.environ['CLOUDNOMICS_REGION']
@@ -23,7 +24,7 @@ def removeExistingFile(fileName):
         os.remove(fileName)
     return
 
-
+@pytest.mark.run(order=1)
 def test_success_creation_of_csvfile():
     # PREPARE
     # clear the output folder
@@ -35,7 +36,7 @@ def test_success_creation_of_csvfile():
     assert os.path.isfile(outputfileNameComplete)
     return
 
-
+@pytest.mark.run(order=2)
 def test_unsuccessful_creation_of_csvfile():
     # PREPARE
     # clear the output folder
@@ -60,7 +61,7 @@ def getDataFrameFromCSV(fullFileName):
         pass
     return dfDynamic
 
-
+@pytest.mark.run(order=3)
 def test_functional_csv_file():
     '''
     Test that the file that exists is a csv file and that it can be opened
@@ -71,7 +72,7 @@ def test_functional_csv_file():
     dfDynamic = getDataFrameFromCSV(fullFileName=outputfileNameComplete)
     assert (isinstance(dfDynamic, pd.DataFrame) and (len(dfDynamic) > 1))
 
-
+@pytest.mark.run(order=4)
 def test_coverage_get_azureVMListWithACUs():
     '''
     Test complete end-2-end happy path of the getAzureVMListWithACUs.py
